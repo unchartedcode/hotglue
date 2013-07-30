4f98f97f1179381f755828d52c72f58f2ad857b9
Nathan Palmer <nate@thinktheweb.com>
2013-07-30 14:18:04 -0400

    Added a newline after the lab script references

--- Source/HotGlue.Reference.LABjs/LABjsScriptReference.cs
+++ Source/HotGlue.Reference.LABjs/LABjsScriptReference.cs
@@ -41,7 +41,8 @@ $LAB
 
             return reference.Name.EndsWith("-module")
                        ? string.Format(".script(\"/hotglue.axd{0}&name={2}\"){1}", relativePath, wait, string.Join("&name=", reference.ReferenceNames))
-                       : string.Format(".script(\"/hotglue.axd{0}\"){1}", relativePath, wait);
+                       : string.Format(".script(\"/hotglue.axd{0}\"){1}", relativePath, wait)
+                       + Environment.NewLine;
         }
 
         public string GenerateFooter()
